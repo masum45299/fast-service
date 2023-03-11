17eccdd8d1901c1bceac42474eb3441bdf8abea6
masum45299 <masum459299@gmail.com>
2023-03-12 00:56:10 +0600

    button

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
                                     <h1>Hi! We Are</h1>
                                     <h1>Creative Designer</h1>
                                     <p>Lorem ipsum dolor sit amet, consectetur   adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
-                                    <button>Order Now</button>
+                                    <button className='order-btn'>Order Now</button>
                                 </div>
                             </div>
                        </div>
